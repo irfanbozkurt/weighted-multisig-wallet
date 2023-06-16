f3e629d543e48e77e7ab4fd5caa0637d149df56d
Irfan Bozkurt <irfanbozkurt123@gmail.com>
2023-06-16 05:59:58 +0300

    fe tweak

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -5,12 +5,12 @@ import { PageHeader } from "antd";
 
 export default function Header() {
   return (
-    <a href="https://github.com/austintgriffith/scaffold-eth/tree/meta-multi-sig" target="_blank" rel="noopener noreferrer">
-      <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="meta-multi-sig example"
-        style={{ cursor: "pointer" }}
-      />
+    <a
+      href="https://buidlguidl.com/builders/0x5F1442eF295BC2Ef0a65b7d49198a34B13c1E3aB"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <PageHeader title="🏗 scaffold-eth" subTitle="weighted-multisig-wallet" style={{ cursor: "pointer" }} />
     </a>
   );
 }
